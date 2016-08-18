2543dbcc4d427e1a4fd2ef6b847092b4d1a35f92
Antoine Choppin <antoine@japonophile.com>
2016-08-18 21:06:08 +0900

    Japanese translation of UI and setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+import os
+
+def get_locales():
+    locale_dir = 'locale'
+    locales = []
+
+    for dirpath, dirnames, filenames in os.walk(locale_dir):
+        for filename in filenames:
+            locales.append(
+                (os.path.join('/usr/share', dirpath),
+                 [os.path.join(dirpath, filename)])
+            )
+
+    return locales
+
+
+setup(name='Make Light',
+      version='1.0',
+      description='Turn code into light',
+      author='Team Kano',
+      author_email='dev@kano.me',
+      url='https://github.com/KanoComputing/make-light',
+      packages=['make_light'],
+      scripts=['bin/make-light'],
+      package_data={'make_light': ['media/css/*']},
+      data_files=[
+      ] + get_locales()
+     )
