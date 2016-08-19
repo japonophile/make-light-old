5e099680cc9dcd6f0e3ff231b7be34d1ed0363ce
Antoine Choppin <antoine@japonophile.com>
2016-08-19 23:17:36 +0900

    Copy locale file

--- copyfiles.py
+++ copyfiles.py
@@ -8,4 +8,5 @@ with open('po/PYPOTFILES') as pypotfiles:
     for ff in ['make_light/logic/challenges.py', 'make_light/paths.py', 'make_light/utils.py']:
         os.system("cp %s /usr/lib/python2.7/dist-packages/%s" % (ff, ff))
     os.system("cp -Rf challenge_groups/locales /usr/share/make-light/challenge_groups/")
+    os.system("cp locale/ja/LC_MESSAGES/make-light.mo /usr/share/locale/ja/LC_MESSAGES/")
 
